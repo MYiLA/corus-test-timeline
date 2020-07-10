91a33c7186369ca8e97301c424f199eb89f37bd1
MYiLA <polus180@ya.ru>
2020-07-10 03:18:34 +0300

    test task completed

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,40 @@
 'use strict';
+$(document).ready(function() {
+
+  $('.timeline').on('click', '.timeline__label', function() {
+    $(this).parents('.timeline').children('.timeline__item').each(function () {
+      $(this).removeClass('timeline__item--active')
+      $(this).removeClass('timeline__item--traversed');
+    })
+
+    $(this).parents('.timeline__item').addClass('timeline__item--active')
+
+    var isActiveFound = false;
+    $(this).parents('.timeline').children('.timeline__item').each(function () {
+      if($(this).hasClass('timeline__item--active')) {
+        isActiveFound = true;
+      } else
+      if (!isActiveFound) {
+        $(this).addClass('timeline__item--traversed');
+      } else {
+        $(this).removeClass('timeline__item--traversed');
+      }
+    })
+  })
+
+  $('.js-add').on('click', function() {
+    $('.timeline').append('<li class="timeline__item"><div class="timeline__progress"></div><span class="timeline__label" tabindex="0">Step</span></li>');
+  })
+  
+  $('.js-delete').on('click', function() {
+    $('.timeline__item:last-child').remove();
+  })
+
+  $('.js-left-center').on('click', function() {
+    $('.timeline').toggleClass('timeline--label-left')
+  })
+
+  $('.js-under-over').on('click', function() {
+    $('.timeline').toggleClass('timeline--under-line')
+  })
+})
