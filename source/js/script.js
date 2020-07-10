bf7dd2248b8b324c8ddae84cd72c11c45f40130d
MYiLA <polus180@ya.ru>
2020-07-10 03:22:10 +0300

    fix js

--- source/js/script.js
+++ source/js/script.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
 
   $('.timeline').on('click', '.timeline__label', function() {
     $(this).parents('.timeline').children('.timeline__item').each(function () {
-      $(this).removeClass('timeline__item--active')
+      $(this).removeClass('timeline__item--active');
       $(this).removeClass('timeline__item--traversed');
-    })
+    });
 
-    $(this).parents('.timeline__item').addClass('timeline__item--active')
+    $(this).parents('.timeline__item').addClass('timeline__item--active');
 
     var isActiveFound = false;
     $(this).parents('.timeline').children('.timeline__item').each(function () {
@@ -19,22 +19,22 @@ $(document).ready(function() {
       } else {
         $(this).removeClass('timeline__item--traversed');
       }
-    })
-  })
+    });
+  });
 
   $('.js-add').on('click', function() {
     $('.timeline').append('<li class="timeline__item"><div class="timeline__progress"></div><span class="timeline__label" tabindex="0">Step</span></li>');
-  })
+  });
   
   $('.js-delete').on('click', function() {
     $('.timeline__item:last-child').remove();
-  })
+  });
 
   $('.js-left-center').on('click', function() {
-    $('.timeline').toggleClass('timeline--label-left')
-  })
+    $('.timeline').toggleClass('timeline--label-left');
+  });
 
   $('.js-under-over').on('click', function() {
-    $('.timeline').toggleClass('timeline--under-line')
-  })
-})
+    $('.timeline').toggleClass('timeline--under-line');
+  });
+});
